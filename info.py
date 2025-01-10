49e7ca99629b4b6e16c5324635798a31f49214a8
Rohith Vijay R <rohithvijay2005@gmail.com>
2025-01-10 19:19:13 +0530

    update

--- info.py
+++ info.py
@@ -90,7 +90,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/moviesandseries41')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/moviesandseries0104')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'request group') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/+tje7bHqwaVozYzVl')
 
 # True Or False
