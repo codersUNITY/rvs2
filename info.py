3560ff97b4bae04215b7fb1acde5cf79c0bda0b4
Rohith Vijay R <rohithvijay2005@gmail.com>
2025-01-10 19:28:53 +0530

    changes

--- info.py
+++ info.py
@@ -88,10 +88,10 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/moviesandseries41')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/moviesandseries0104')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'request group') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/+tje7bHqwaVozYzVl')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/vj_bot_disscussion')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/vj_botz')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/kingvj01')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
